e96db6c550c2f5a6928f9957320a121a26af8c63
Chanti <chvenkatesh.1118@gmail.com>
2022-04-11 07:17:10 +0530

    24-03

--- commands.sh
+++ commands.sh
@@ -20,3 +20,50 @@ tar
 grep
 su
 awk
+ ls
+ls -l
+ls -a
+cd
+pwd
+cat
+touch
+mkdir
+mkdir -p
+cp
+mv
+rmdir
+rm
+rm -rf
+uname
+uname -r
+uname -a
+sudo
+su
+top
+free
+date
+date +%FOR
+timedatectl
+hwclock
+command --help
+useradd
+userdel
+usermod
+chage
+chmod
+chown
+yum
+rpm -qa
+rpm -i
+df -h
+du
+blkid
+mount
+umount
+ln
+ln -s
+ssh
+w
+whoami
+id
+journalctl
